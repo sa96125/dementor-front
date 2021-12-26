8927882f46f1b54967fba839e324997fec8be002
sa96125 <sa96125@gmail.com>
2021-12-26 23:03:36 +0900

    add log-out page router

---
+++ src/client/routers/logged-out-router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CreateAccount } from "../pages/create-account";
+import { Login } from "../pages/login";
+
+export const LoggedOutRouter: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+      </Routes>
+    </Router>
+  );
+};
