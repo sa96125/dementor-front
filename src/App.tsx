bab0b0f9ebed8313c70cd3d46659cc23c9686500
sa96125 <sa96125@gmail.com>
2021-12-26 23:05:07 +0900

    add login logic

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useReactiveVar } from "@apollo/client";
+import { LoggedInRouter } from "./client/routers/logged-in-router";
+import { LoggedOutRouter } from "./client/routers/logged-out-router";
+import { isLoggedInVar } from "./service/plugins/apolloAdapter";
+
+function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
+}
+
+export default App;
