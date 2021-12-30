053c6a2421dd1ef8730a7607d3c178ee608899d2
sa96125 <sa96125@gmail.com>
2021-12-30 23:55:43 +0900

    add HelmetProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import { ApolloProvider } from "@apollo/client";
-import React from "react";
-import ReactDOM from "react-dom";
-import { client } from "./service/plugins/apolloAdapter";
-import App from "./App";
-import "./client/assets/css/index.css";
+import { ApolloProvider } from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { client } from './service/plugins/apolloAdapter';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
+import './client/assets/css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
